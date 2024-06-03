c6a3068e2613aaf1cc4c649d491021bfb012653c
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-06-04 00:12:47 +0600

    pagination added

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@
 
 from rest_framework import viewsets
 from .models import BlogPost
+from .paginations import BlogPagination
 from .serializers import BlogPostSerializer
+
 
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    pagination_class = BlogPagination
