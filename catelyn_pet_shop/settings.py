403b0a5289a909d0c59defee47289d5c654d4fc2
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-08-27 01:21:34 +0600

    bugfix on s3 connection

--- catelyn_pet_shop/settings.py
+++ catelyn_pet_shop/settings.py
@@ -212,8 +212,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
-AWS_S3_CUSTOM_DOMAIN = None
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
