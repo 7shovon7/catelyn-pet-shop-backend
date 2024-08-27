8849debd3e96fc9e22a4758a203e2a595101cff8
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-08-27 13:40:13 +0600

    custom fields added

--- shop_settings/admin.py
+++ shop_settings/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, CustomField
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at')
-    search_fields = ('title',)
+    list_display = ['title']
+    search_fields = ['title']
+
+
+@admin.register(CustomField)
+class CustomFieldAdmin(admin.ModelAdmin):
+    list_display = ['name', 'key', 'is_required', 'is_private']
+    search_fields = ['name', 'key']
+    readonly_fields = ['id', 'key']
+
+    fieldsets = [
+        ("Basic Information", {"fields": ['id', 'name', 'is_required', 'is_private']}),
+        ("Advanced Settings", {"fields": ['key'], 'classes': ['collapse']}),
+    ]
--- shop_settings/models.py
+++ shop_settings/models.py
@@ -16,3 +16,26 @@ class Category(models.Model):
         if self.image:
             self.image = optimize_image_in_upload_model(self.image, desired_height=300)
         super().save(*args, **kwargs)
+        
+        
+class CustomField(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    key = models.CharField(max_length=100, editable=False, null=True)
+    is_required = models.BooleanField(default=False)
+    is_private = models.BooleanField(default=False)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.key:
+            base_key = self.name.lower().replace(' ', '-')
+            key = base_key
+            counter = 1
+            while CustomField.objects.filter(key=key).exists():
+                key = f"{base_key}-{counter}"
+                counter += 1
+                
+            self.key = key
+        
+        return super().save(*args, **kwargs)
